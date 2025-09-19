431b5d30ac5cbe1d1527e3e353cd01772f8ed8de
Cristian Alonso Matos Xol <24470034@campeche.tecnm.mx>
2025-09-19 11:01:12 -0600

    Versión que utiliza VerticalStackLaout Como elemento raiz

---
+++ ContadorManual.Gui/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace ContadorManual.Gui
+{
+    public partial class MainPage : ContentPage
+        {
+            private int _conteo;
+            public MainPage()
+            {
+            //_conteo lleva el coteo de la aplicación
+                InitializeComponent();
+                _conteo = 0;
+                ConteoLabel.Text=_conteo.ToString();
+            }
+
+            private void Button_Clicked(object sender, EventArgs e)
+            {
+
+
+            _conteo++;
+           
+            ConteoLabel.Text = _conteo.ToString();
+
+            }
+
+            private void Button_Clicked_1(object sender, EventArgs e)
+            {
+
+            _conteo = 0;
+            ConteoLabel.Text = _conteo.ToString();
+        }
+    }
+}
